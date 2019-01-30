63077202b78c4d79bd9617254956699f040d1ba3
Mayankraj23 <raj.6@iitj.ac.in>
2019-01-30 19:42:15 +0530

    Set the template view

--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,12 @@
-from django.views.generic import TemplateView
+from __future__ import absolute_import
+from django.views.generic.base import TemplateView
+from blogs.models import blog
 
 
 class BlogHome(TemplateView):
     template_name = 'blog.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(BlogHome, self).get_context_data(**kwargs)
+        context['articles'] = blog.objects.all()
+        return context
