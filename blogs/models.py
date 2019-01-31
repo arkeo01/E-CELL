b428e1d988a30cd24178fe52e2c0792293a5fa11
Mayankraj23 <raj.6@iitj.ac.in>
2019-01-31 22:41:13 +0530

    Added richtext editor

--- blogs/models.py
+++ blogs/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+from tinymce.models import HTMLField
 
 class blog(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField()
-    body = models.TextField()
+    body = HTMLField()
     date = models.DateField(auto_now_add=True)
 
     def __str__(self):
--- e_cell/urls.py
+++ e_cell/urls.py
@@ -19,6 +19,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'^tinymce/', include('tinymce.urls')),
     path('', TemplateView.as_view(template_name="ecell_home.html")),
     path('blog/', include('blogs.urls')),
 ]
