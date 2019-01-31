36a024b531e26d1a8aa7a207a091173c56178afa
Mayankraj23 <raj.6@iitj.ac.in>
2019-01-31 22:24:33 +0530

    Updated models

--- blogs/models.py
+++ blogs/models.py
@@ -5,7 +5,7 @@ class blog(models.Model):
     title = models.CharField(max_length=100)
     slug = models.SlugField()
     body = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title
